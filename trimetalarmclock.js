610c933657def79bb964741fbe84fe729258ea96
Christopher Stokes <christopherstokes@gmail.com>
2015-06-02 03:02:21 -0700

    made changes

--- trimetalarmclock.js
+++ trimetalarmclock.js
@@ -17,9 +17,17 @@ var commandArgs = process.argv.slice(2),
 		"ns": 193,
 		"cl": 194
 	},
-	trainKeys = ["blue", "green", "red", "yellow", "wes", "ns", "cl"]
+	trainKeys = ["blue", "green", "red", "yellow", "wes", "ns", "cl"],
+	waiting = '';
+
+function processCommands() {
+	for (var i = 0; i < commandArgs.length; i++) {
+		// add in functionality
+	}
+}
 
 function checkBus() {
+	process.stdout.write("\u001b[2J\u001b[0;0H");
 	request({
 		url: url,
 		json: true
@@ -28,8 +36,7 @@ function checkBus() {
 		if (!error && response.statusCode === 200) {
 			var resultsData = body.resultSet; // Print the json response
 			var currentTime = new Date(resultsData.queryTime);
-			var timeUntil,
-				busOrTrain
+			var timeUntil;
 
 			function checkTime(currentTime, arrivalTime) {
 				return (arrivalTime / 1000 - currentTime / 1000) / 60;
@@ -44,11 +51,9 @@ function checkBus() {
 			function getTrain(oldRoute) {
 				for (var i = 0; i < trainKeys.length; i++) {
 					if (oldRoute == trains[trainKeys[i]]) {
-						busOrTrain = " (train)";
 						return trainKeys[i];
 					}
 				}
-				busOrTrain = " (bus)";
 			}
 
 
@@ -67,22 +72,22 @@ function checkBus() {
 						if (resultsData.arrival[i].status == "estimated") {
 							timeUntil = Math.floor(checkTime(currentTime, new Date(resultsData.arrival[i].estimated)));
 							if (timeUntil <= timer) {
-								console.log("You have " + timeUntil + " minutes to make the " + getThatTrain + busOrTrain);
+								console.log("You have " + timeUntil + " minutes to make the " + getThatTrain);
 								shell.exec('aplay bell.wav');
 							} else {
-								console.log("...");
+								console.log(waiting += "...");
+								console.log("You still have " + timeUntil + " minutes to make the " + getThatTrain);
 							}
 						} else {
 							timeUntil = Math.floor(checkTime(currentTime, new Date(resultsData.arrival[i].scheduled)));
 							if (timeUntil <= timer) {
-								console.log("You have " + timeUntil + " minutes to make the " + getThatTrain + busOrTrain);
+								console.log("You have " + timeUntil + " minutes to make the " + getThatTrain);
 								shell.exec('aplay bell.wav');
 
 							} else {
-								console.log("...");
+								console.log(waiting += "...");
+								console.log("You still have " + timeUntil + " minutes to make the " + getThatTrain);
 							}
-
-
 						}
 					}
 				}
